303777fa0bfa8a2434c0627ef3c1da65e8950b2f
Elliot Saba <staticfloat@gmail.com>
2014-03-30 16:33:51 -0700

    Add PanoramaTest

---
+++ PanoramaTest/PanoramaTest/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PanoramaTest.Resources;
+
+namespace PanoramaTest
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
